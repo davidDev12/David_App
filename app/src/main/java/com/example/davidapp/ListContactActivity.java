cbea32d26d6280af8b95301dba7a1441fe54efd5
david <you@example.comaitdaoudelhoucine@gmail.com>
2021-03-17 12:07:00 +0100

    git contact from phone

--- app/src/main/java/com/example/davidapp/ListContactActivity.java
+++ app/src/main/java/com/example/davidapp/ListContactActivity.java
@@ -1,24 +1,15 @@
 package com.example.davidapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.database.MatrixCursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -35,11 +26,11 @@ public class ListContactActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_contact);
         ls = findViewById(R.id.list2);
         list = new ArrayList<Item>();
-       list.add(new Item("houcine",/*"daoud",*/"+212636520766"));
-        list.add(new Item("Mohamed",/*"daoud",*/"+212677560566"));
-        list.add(new Item("Ali",/*"daoud",*/"+212610748839"));
-        mAdapter = new mAdapter( getApplicationContext(),list);
-       ls.setAdapter(mAdapter);
+     //  list.add(new Item("houcine",/*"daoud",*/"+212636520766"));
+        //list.add(new Item("Mohamed",/*"daoud",*/"+212677560566"));
+       // list.add(new Item("Ali",/*"daoud",*/"+212610748839"));
+       // mAdapter = new mAdapter( getApplicationContext(),list);
+      // ls.setAdapter(mAdapter);
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)== PackageManager.PERMISSION_DENIED){
             ActivityCompat.requestPermissions(ListContactActivity.this, new String[]{Manifest.permission.READ_CONTACTS},1);
         }
@@ -54,18 +45,22 @@ public class ListContactActivity extends AppCompatActivity {
     }
 
 
-    class loadContacts extends AsyncTask<Void,Void,ArrayList<String>> {
+    class loadContacts extends AsyncTask<Void, Void, ArrayList<Item>> {
 
         @Override
-        protected ArrayList<String> doInBackground(Void... voids) {
-            ArrayList Contacts = new ArrayList<String>();
+        protected ArrayList<Item> doInBackground(Void... voids) {
+            list = new ArrayList<Item>();
+          //  ArrayList Contacts = new ArrayList<String>();
             Cursor q = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
             while(q.moveToNext()){
                 String nom = q.getString(q.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
                 String num = q.getString(q.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                Contacts.add(nom+" : "+ num);
+                list.add(new Item(nom,num));
+
+                //list.add(new itemnom+" : "+ num);
+
             }
-            return Contacts;
+            return  list;
         }
         ProgressDialog pDialog;
         @Override
@@ -78,11 +73,11 @@ public class ListContactActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(ArrayList<String> strings) {
-            //mAdapter = new mAdapter( getApplicationContext(), Contacts);
+        protected void onPostExecute(ArrayList<Item> strings) {
+            mAdapter = new mAdapter( getApplicationContext(), list);
 
-           ArrayAdapter<String> adap = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1,strings);
-            ls.setAdapter(adap);
+          // ArrayAdapter<String> adap = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1,strings);
+            ls.setAdapter(mAdapter);
             pDialog.hide();
         }
     }
